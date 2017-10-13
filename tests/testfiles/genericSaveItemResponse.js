78ad0caed86b30edcf7e135de58cccd95601f5b0
flobee <flobee@r61.local>
2017-10-13 20:16:33 +0200

    Adds tests for the gerneric DTO/DAO objects

---
+++ tests/testfiles/genericSaveItemResponse.js
@@ -0,0 +1,14 @@
+{
+    "jsonrpc": "2.0",
+    "result": {
+        "server": {
+            "code": 0,
+            "message": "Success"
+        },
+        "item": {
+            "id": 3,
+            "name": "name 3"
+        }
+    },
+    "id": 0
+}
