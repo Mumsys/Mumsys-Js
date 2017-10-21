e3fd98a9b3d8bc98a21911c39332ac2eca874704
flobee <flobee@r61.local>
2017-10-21 18:24:15 +0200

    Try to enable travis"

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,75 @@
+/*global module:false*/
 module.exports = function(grunt) {
+
     // Project configuration.
     grunt.initConfig({
         qunit: {
-            files: ['tests/run-tests-development.html', 'tests/run-tests-standalone-version.html', 'tests/run-tests-minified-version.html' ]
+            'mumsys-js': [
+                'tests/run-tests-development.html', 
+                'tests/run-tests-standalone-version.html', 
+                'tests/run-tests-minified-version.html'
+            ]
+        },
+        jshint: {
+            'mumsys-js': ['src/**/*.js', 'Grunfile.js', 'tests/testfiles/**/*.js'],
+            options: {
+                jquery: true,
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                unused: false,
+                onevar: true,
+                boss: true,
+                eqnull: true,
+                browser: true,
+                devel: true,
+                trailing: true,
+                white:  false,
+                maxcomplexity: 4,
+                globals: {
+                    Kort: true,
+                    Ext: true,
+                    L: true,
+                    UrlLib: true,
+                    urlLib: true,
+                    asyncTest: true,
+                    api_test: true,
+                    deepEqual: true,
+                    equal: true,
+                    expect: true,
+                    module: true,
+                    notDeepEqual: true,
+                    notEqual: true,
+                    notStrictEqual: true,
+                    ok: true,
+                    QUnit: true,
+                    raises: true,
+                    start: true,
+                    stop: true,
+                    strictEqual: true,
+                    test: true,
+                    testSkip: true
+                }
+            }
         }
     });
 
-    // Load plugin
+    // Load plugins
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-qunit');
 
-    // Task to run tests
-    grunt.registerTask('test', 'qunit');
+    // Default task.
+    // grunt.registerTask('default', 'jshint:mumsys-js');
+
+    // Linting task.
+    // grunt.registerTask('lint', 'jshint:mumsys-js');
+
+    // Travis CI task.
+    //grunt.registerTask('travis', 'jshint:mumsys-js qunit:mumsys-js');
+    grunt.registerTask('travis', 'qunit:mumsys-js');
 };
